83289171145838133c6d14af8965c71379aa898f
John Sankey Job <johnsankeyjob@gmail.com>
2026-09-12 18:46:05 +0530

    build(android): configure compileSdk 37, CMake 3.22.1, and cross-drive incremental flags

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.mywallet.my_wallet"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 37
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
